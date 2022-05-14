d03984e1e70ebeb5329d0675220d34d0a5e24b0b
Varun Kumar <varun@Varun-MacBook-Pro.local>
2022-05-14 13:28:41 +0530

    Add Mockbee

---
+++ src/backend/db/categories.js
@@ -0,0 +1,34 @@
+import { v4 as uuid } from "uuid";
+
+/**
+ * Category Database can be added here.
+ * You can add category of your wish with different attributes
+ * */
+
+export const categories = [
+  {
+    _id: "ALL",
+    categoryName: "All",
+    description: "",
+  },
+  {
+    _id: "category1",
+    categoryName: "Vertical Gardens",
+    description: "",
+  },
+  {
+    _id: "category2",
+    categoryName: "Balcony Gardens",
+    description: "",
+  },
+  {
+    _id: "category3",
+    categoryName: "Garden Makeovers",
+    description: "",
+  },
+  {
+    _id: "category4",
+    categoryName: "Open Gardens",
+    description: "",
+  },
+];
---
+++ src/backend/db/videos.js
@@ -0,0 +1,67 @@
+/**
+ * Videos Database can be added here.
+ * You can add videos of your wish with different attributes
+ * */
+
+export const videos = [
+  {
+    _id: "Wo5dMEP_BbI",
+    title: "A beautiful waterfall and duck lake ",
+    thumbnail:
+      "https://res.cloudinary.com/duepvqb5b/image/upload/v1648206129/gardenkart-products/products/annie-spratt-vsnISjCT2hY-unsplash_vz0tt4.jpg",
+    src: "https://www.youtube.com/embed/qH-c7NJRAco",
+    description: "Garden Decoration",
+    creator: "Soham Shah",
+    categoryId: "category2",
+  },
+  {
+    _id: "F_Riqjdh2oM",
+    title: "Landscape Design Ideas",
+    thumbnail:
+      "https://res.cloudinary.com/duepvqb5b/image/upload/v1648206129/gardenkart-products/products/annie-spratt-vsnISjCT2hY-unsplash_vz0tt4.jpg",
+    src: "https://www.youtube.com/embed/rkfr4VatFs8",
+    creator: "Sentdex",
+    description: "Contemporary Garden Designs",
+    categoryId: "category3",
+  },
+  {
+    _id: "F_Riqjdh2oN",
+    title: "Small balcony makeover",
+    thumbnail:
+      "https://res.cloudinary.com/duepvqb5b/image/upload/v1648206129/gardenkart-products/products/annie-spratt-vsnISjCT2hY-unsplash_vz0tt4.jpg",
+    src: "https://www.youtube.com/embed/urpqClU-7qc",
+    creator: "Sentdex",
+    description: "Zen corner | balcony organisation",
+    categoryId: "category2",
+  },
+  {
+    _id: "F_Riqjdh2oO",
+    title: "Balcony Garden",
+    thumbnail:
+      "https://res.cloudinary.com/duepvqb5b/image/upload/v1648206129/gardenkart-products/products/annie-spratt-vsnISjCT2hY-unsplash_vz0tt4.jpg",
+    src: "https://www.youtube.com/embed/yGf9PBa79V4",
+    creator: "Sentdex",
+    description: "balcony organisation",
+    categoryId: "category4",
+  },
+  {
+    _id: "F_Riqjdh2oP",
+    title: "DIY vertical hanging garden ",
+    thumbnail:
+      "https://res.cloudinary.com/duepvqb5b/image/upload/v1648206129/gardenkart-products/products/annie-spratt-vsnISjCT2hY-unsplash_vz0tt4.jpg",
+    src: "https://www.youtube.com/embed/smcByjzCKk8",
+    creator: "Sentdex",
+    description: "growing Portulaca (Mossrose)",
+    categoryId: "category1",
+  },
+  {
+    _id: "F_Riqjdh2oQ",
+    title: "Amazing Vertical Garden",
+    thumbnail:
+      "https://res.cloudinary.com/duepvqb5b/image/upload/v1648206129/gardenkart-products/products/annie-spratt-vsnISjCT2hY-unsplash_vz0tt4.jpg",
+    src: "https://www.youtube.com/embed/DgHvEUfCAEU",
+    creator: "Sentdex",
+    description: " Using Plastic Bottles",
+    categoryId: "category2",
+  },
+];
